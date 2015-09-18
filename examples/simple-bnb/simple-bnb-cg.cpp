4f090094b0a65f2f1b987f3ee2ccc8d6d7b515d9
Ashutosh Mahajan <amahajan@iitb.ac.in>
2015-09-18 03:02:23 +0000

    simplified branch-and-bound with and without native derivatives
    git-svn-id: https://repo.anl-external.org/repos/minotaur@159 1b4cc879-78bd-47a1-b9a5-4e5bb735bf01

--- examples/simple-bnb/simple-bnb-cg.cpp
+++ examples/simple-bnb/simple-bnb-cg.cpp
@@ -14,13 +14,16 @@
 #include <iostream>
 
 #include "AMPLInterface.h"
+#include "AMPLJacobian.h"
+#include "AMPLHessian.h"
 #include "MinotaurConfig.h"
 #include "BndProcessor.h"
 #include "BranchAndBound.h"
 #include "Environment.h"
-#include "FilterSQPEngine.h"
 #include "IntVarHandler.h"
+#include "IpoptEngine.h"
 #include "NodeIncRelaxer.h"
+#include "Objective.h"
 #include "Option.h"
 #include "Problem.h"
 #include "Relaxation.h"
@@ -35,15 +38,28 @@ int main(int argc, char** argv)
 {
   EnvPtr env = (EnvPtr) new Environment();
   HandlerVector handlers;
+  double objsense = 1.0;
   int err = 0;
 
   // Start timer and read the problem
   env->startTimer(err); assert(err==0);
   env->getOptions()->findBool("use_native_cgraph")->setValue(true);
+  env->getOptions()->findBool("expand_quad")->setValue(false);
+  env->getOptions()->findDouble("bnb_time_limit")->setValue(3600);
+
   MINOTAUR_AMPL::AMPLInterface* iface =
     new MINOTAUR_AMPL::AMPLInterface(env, "bnb");
   ProblemPtr p = iface->readInstance(argv[1]);
-  p->setNativeDer();
+  if (Maximize == p->getObjective()->getObjectiveType()) {
+    p->negateObj();
+    objsense = -1.0;
+  }
+  //MINOTAUR_AMPL::AMPLJacobianPtr jac = (MINOTAUR_AMPL::AMPLJacobianPtr) new MINOTAUR_AMPL::AMPLJacobian(iface);
+  //p->setJacobian(jac);
+  //MINOTAUR_AMPL::AMPLHessianPtr hess = (MINOTAUR_AMPL::AMPLHessianPtr) new MINOTAUR_AMPL::AMPLHessian(iface);
+  //p->setHessian(hess);
+
+  //p->setNativeDer();
 
   // create branch-and-bound object
   BranchAndBound *bab = new BranchAndBound(env, p);
@@ -53,7 +69,7 @@ int main(int argc, char** argv)
   handlers.push_back(v_hand);
 
   // setup engine for solving relaxations and branching
-  EnginePtr e = (FilterSQPEnginePtr) new FilterSQPEngine(env);
+  EnginePtr e = (IpoptEnginePtr) new IpoptEngine(env);
   ReliabilityBrancherPtr rel_br = (ReliabilityBrancherPtr) new
                                   ReliabilityBrancher(env, handlers);
   rel_br->setEngine(e);
@@ -66,6 +82,15 @@ int main(int argc, char** argv)
   // node relaxer
   NodeIncRelaxerPtr nr = (NodeIncRelaxerPtr) new NodeIncRelaxer(env, handlers);
   RelaxationPtr rel = (RelaxationPtr) new Relaxation(p);
+  rel->setNativeDer();
+
+  //rel->calculateSize();
+  //if (rel->isQP()) {
+  //  rel->setNativeDer();
+  //} else {
+  //  rel->setJacobian(jac);
+  //  rel->setHessian(hess);
+  //}
   nr->setRelaxation(rel);
   nr->setEngine(e);
   nr->setModFlag(false);
@@ -75,8 +100,10 @@ int main(int argc, char** argv)
   // start solving
   bab->solve();
   bab->writeStats(std::cout);
-  bab->getSolution()->writePrimal(std::cout);
-  std::cout << "best solution value = " << bab->getUb() << std::endl;
+  //bab->getSolution()->writePrimal(std::cout);
+  std::cout << "best solution value = " << objsense*bab->getUb() << std::endl
+    << "status of branch-and-bound: "
+    << getSolveStatusString(bab->getStatus()) << std::endl;
 
   //finish
   delete iface;
