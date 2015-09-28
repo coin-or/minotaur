8bb33c3dcdfebcf8dd5bb19ace52d64227a76e10
Ashutosh Mahajan <amahajan@iitb.ac.in>
2015-09-28 17:53:02 +0000

    Fixed simple example of qg.
    git-svn-id: https://repo.anl-external.org/repos/minotaur@165 1b4cc879-78bd-47a1-b9a5-4e5bb735bf01

--- examples/simple-qg/simple-qg-nocg.cpp
+++ examples/simple-qg/simple-qg-nocg.cpp
@@ -16,20 +16,21 @@
 #include "MinotaurConfig.h"
 #include "BranchAndBound.h"
 #include "Environment.h"
-#include "FilterSQPEngine.h"
 #include "IntVarHandler.h"
+#include "IpoptEngine.h"
 #include "LinearHandler.h"
 #include "LPProcessor.h"
 #include "NodeIncRelaxer.h"
+#include "Objective.h"
 #include "Option.h"
 #include "OsiLPEngine.h"
 #include "Problem.h"
 #include "QGHandler.h"
 #include "Relaxation.h"
 #include "ReliabilityBrancher.h"
-#include "Solution.h"
 #include "Timer.h"
-#include "TreeManager.h"
+#include "AMPLHessian.h"
+#include "AMPLJacobian.h"
 #include "AMPLInterface.h"
 
 using namespace Minotaur;
@@ -38,19 +39,31 @@ int main(int argc, char** argv)
 {
   EnvPtr env = (EnvPtr) new Environment();
   HandlerVector handlers;
+  double objsense = 1.0;
   int err = 0;
 
   // Start timer and read the problem
   env->startTimer(err); assert(err==0);
-  env->getOptions()->findBool("use_native_cgraph")->setValue(true);
+  env->getOptions()->findBool("use_native_cgraph")->setValue(false);
+  env->getOptions()->findBool("expand_quad")->setValue(false);
+  env->getOptions()->findDouble("bnb_time_limit")->setValue(3600);
+
   MINOTAUR_AMPL::AMPLInterface* iface =
-    new MINOTAUR_AMPL::AMPLInterface(env, "bnb");
+    new MINOTAUR_AMPL::AMPLInterface(env, "qg");
   ProblemPtr p = iface->readInstance(argv[1]);
-  p->setNativeDer();
+  if (Maximize == p->getObjective()->getObjectiveType()) {
+    p->negateObj();
+    objsense = 1.0;
+  }
+  MINOTAUR_AMPL::AMPLJacobianPtr jac = (MINOTAUR_AMPL::AMPLJacobianPtr) new MINOTAUR_AMPL::AMPLJacobian(iface);
+  p->setJacobian(jac);
+  MINOTAUR_AMPL::AMPLHessianPtr hess = (MINOTAUR_AMPL::AMPLHessianPtr) new MINOTAUR_AMPL::AMPLHessian(iface);
+  p->setHessian(hess);
+
 
   // create branch-and-bound object
   BranchAndBound *bab = new BranchAndBound(env, p);
-  EnginePtr nlp_e = (FilterSQPEnginePtr) new FilterSQPEngine(env);
+  EnginePtr nlp_e = (IpoptEnginePtr) new IpoptEngine(env);
   EnginePtr e = (OsiLPEnginePtr) new OsiLPEngine(env);
 
   // setup handlers
@@ -83,8 +96,10 @@ int main(int argc, char** argv)
   // start solving
   bab->solve();
   bab->writeStats(std::cout);
-  bab->getSolution()->writePrimal(std::cout);
-  std::cout << "best solution value = " << bab->getUb() << std::endl;
+  //bab->getSolution()->writePrimal(std::cout);
+  std::cout << "best solution value = " << objsense*bab->getUb() << std::endl
+    << "status of branch-and-bound: "
+    << getSolveStatusString(bab->getStatus()) << std::endl;
 
   //finish
   delete iface;
