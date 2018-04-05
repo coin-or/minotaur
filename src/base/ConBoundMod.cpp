45d073bb2650de9fa9cc8c38b52a29934c5eca58
meenarli <kukku.meenarli@gmail.com>
2018-04-05 12:43:15 +0530

    Functionality for constraint bounds modification

---
+++ src/base/ConBoundMod.cpp
@@ -0,0 +1,247 @@
+// 
+//     MINOTAUR -- It's only 1/2 bull
+// 
+//     (C)opyright 2009 - 2017 The MINOTAUR Team.
+// 
+
+/**
+ * \file ConBoundMod.cpp
+ * \brief Implement the Modification class ConBoundMod, that is used to store
+ * modifications to a bound on a constraint.
+ * \author Meenarli Sharma, Indian Institute of Technology Bombay
+ */
+
+#include <iostream>
+
+#include "MinotaurConfig.h"
+#include "Engine.h"
+#include "Problem.h"
+#include "Relaxation.h"
+#include "ConBoundMod.h"
+#include "Constraint.h"
+#include "NonlinearFunction.h"
+
+
+using namespace Minotaur;
+
+
+ConBoundMod::ConBoundMod(ConstraintPtr con, BoundType lu, double new_val)
+  : lu_(lu),
+    newVal_(new_val),
+    con_(con)
+{
+  switch (lu) {
+   case (Lower):
+     oldVal_ = con->getLb();
+     break;
+   case (Upper):
+     oldVal_ = con->getUb();
+     break;
+   default:
+     break;
+  }
+}
+
+
+ConBoundMod::~ConBoundMod()
+{
+  con_.reset();
+}
+
+/// TO DO
+//ModificationPtr ConBoundMod::fromRel(RelaxationPtr rel, ProblemPtr) const
+//{
+  //ConBoundModPtr mod = (ConBoundModPtr) new ConBoundMod(
+                                        //rel->getOriginalVar(con_),
+                                        //lu_, newVal_);
+  //mod->oldVal_ = oldVal_;
+  //return mod;
+//}
+
+
+ConstraintPtr ConBoundMod::getCon() const
+{
+  return con_;
+}
+
+
+BoundType ConBoundMod::getLU() const
+{
+  return lu_;
+}
+
+
+double ConBoundMod::getNewVal() const
+{
+  return newVal_;
+}
+
+
+void ConBoundMod::applyToProblem(ProblemPtr problem) 
+{
+  problem->changeBound(con_, lu_, newVal_);
+}
+
+/// TO DO
+//ModificationPtr ConBoundMod::toRel(ProblemPtr, RelaxationPtr rel) const
+//{
+  //ConBoundModPtr mod = (ConBoundModPtr) new ConBoundMod(
+                                        //rel->getRelaxationVar(con_),
+                                        //lu_, newVal_);
+  //mod->oldVal_ = oldVal_;
+  //return mod;
+//}
+
+
+void ConBoundMod::undoToProblem(ProblemPtr problem) 
+{
+  problem->changeBound(con_, lu_, oldVal_);
+}
+
+
+void ConBoundMod::write(std::ostream &out) const
+{
+  out << "con bound mod: "
+      << "con name = " << con_->getName()
+      << " bound type = ";
+  if (lu_==Lower) {
+    out << "lb"; 
+  } else {
+    out << "ub";
+  }
+  out << " old value = " << oldVal_
+      << " new value = " << newVal_
+      << std::endl;
+}
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+
+ConBoundMod2::ConBoundMod2(ConstraintPtr con, double new_lb, double new_ub) 
+  : newLb_(new_lb),
+    newUb_(new_ub),
+    con_(con)
+{
+  oldLb_ = con->getLb();
+  oldUb_ = con->getUb();
+}
+
+
+ConBoundMod2::~ConBoundMod2()
+{
+  con_.reset();
+}
+
+/// TO DO
+//ModificationPtr ConBoundMod2::fromRel(RelaxationPtr rel, ProblemPtr) const
+//{
+  //ConBoundMod2Ptr mod = (ConBoundMod2Ptr) new ConBoundMod2(
+                                          //rel->getOriginalVar(con_),
+                                          //newLb_, newUb_);
+  //mod->oldLb_ = oldLb_;
+  //mod->oldUb_ = oldUb_;
+
+  //return mod;
+//}
+
+
+ConstraintPtr ConBoundMod2::getCon() const
+{
+  return con_;
+}
+
+
+double ConBoundMod2::getNewLb() const
+{
+  return newLb_;
+}
+
+
+double ConBoundMod2::getNewUb() const
+{
+  return newUb_;
+}
+
+
+void ConBoundMod2::applyToProblem(ProblemPtr problem)
+{
+  problem->changeBound(con_, newLb_, newUb_);
+}
+
+//// TO DO
+//ModificationPtr ConBoundMod2::toRel(ProblemPtr, RelaxationPtr rel) const
+//{
+  //ConBoundMod2Ptr mod = (ConBoundMod2Ptr) new ConBoundMod2(
+                                          //rel->getRelaxationVar(con_),
+                                          //newLb_, newUb_);
+  //mod->oldLb_ = oldLb_;
+  //mod->oldUb_ = oldUb_;
+
+  //return mod;
+//}
+
+
+void ConBoundMod2::undoToProblem(ProblemPtr problem)
+{
+  problem->changeBound(con_, oldLb_, oldUb_);
+}
+
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+
+ConMod::ConMod(ConstraintPtr con, NonlinearFunctionPtr new_nlf) 
+  : newNlf_(new_nlf),
+    con_(con)
+{
+  oldNlf_ = con->getNonlinearFunction();
+}
+
+
+ConMod::~ConMod()
+{
+  con_.reset();
+}
+
+ConstraintPtr ConMod::getCon() const
+{
+  return con_;
+}
+
+
+NonlinearFunctionPtr ConMod::getNewNlf() const
+{
+  return newNlf_;
+}
+
+
+void ConMod::applyToProblem(ProblemPtr problem) 
+{
+  problem->changeConstraint(con_, newNlf_);
+}
+
+/// TO DO
+//ModificationPtr ConBoundMod::toRel(ProblemPtr, RelaxationPtr rel) const
+//{
+  //ConBoundModPtr mod = (ConBoundModPtr) new ConBoundMod(
+                                        //rel->getRelaxationVar(con_),
+                                        //lu_, newVal_);
+  //mod->oldVal_ = oldVal_;
+  //return mod;
+//}
+
+
+void ConMod::undoToProblem(ProblemPtr problem) 
+{
+  problem->changeConstraint(con_, oldNlf_);
+}
+
+
+// Local Variables: 
+// mode: c++ 
+// eval: (c-set-style "k&r") 
+// eval: (c-set-offset 'innamespace 0) 
+// eval: (setq c-basic-offset 2) 
+// eval: (setq fill-column 78) 
+// eval: (auto-fill-mode 1) 
+// eval: (setq column-number-mode 1) 
+// eval: (setq indent-tabs-mode nil) 
+// End:
---
+++ src/base/ConBoundMod.h
@@ -0,0 +1,208 @@
+// 
+//     MINOTAUR -- It's only 1/2 bull
+// 
+//     (C)opyright 2009 - 2017 The MINOTAUR Team.
+// 
+
+/**
+ * \file ConBoundMod.h
+ * \brief Declare the class ConBoundMod. It is used to save modifications in a
+ * bound of a constraint. Also declare ConBoundMod2 that is used to change both
+ * bounds of a constraint. Also declare ConMod that is used to change
+ * nonlinear part of the constraint
+ * \author Meenarli Sharma, Indian Institute of Technology Bombay
+ */
+
+
+#ifndef MINOTAURCONBOUNDMOD_H
+#define MINOTAURCONBOUNDMOD_H
+
+#include "Modification.h"
+#include "NonlinearFunction.h"
+
+namespace Minotaur {
+  class Engine;
+  class ConBoundMod;
+  typedef boost::shared_ptr <Engine> EnginePtr;
+  typedef boost::shared_ptr<ConBoundMod> ConBoundModPtr;
+  typedef std::vector <ConBoundModPtr> ConBoundModVector;
+  typedef ConBoundModVector::iterator ConBoundModIter;
+  typedef ConBoundModVector::const_iterator ConBoundModConstIter;
+
+
+  /// Modification of a single bound on a constraint
+  class ConBoundMod : public Modification {
+    public:
+      /// Construct.
+      ConBoundMod(ConstraintPtr con, BoundType lu, double new_val);
+
+      /// Destroy.
+      ~ConBoundMod();
+
+      // base class method.
+      ModificationPtr fromRel(RelaxationPtr, ProblemPtr ) const
+      {return ModificationPtr();};
+
+      // base class method.
+      ModificationPtr toRel(ProblemPtr, RelaxationPtr) const
+      {return ModificationPtr();};
+
+      /// Get the constraint whose bound is changed.
+      ConstraintPtr getCon() const;
+
+      /// Get the type of bound that is changed: lower or upper.
+      BoundType getLU() const;
+
+      /// Get new value of the bound.
+      double getNewVal() const;
+
+      // Implement Modification::applyToProblem().
+      void applyToProblem(ProblemPtr problem);
+
+      // Implement Modification::undoToProblem().
+      void undoToProblem(ProblemPtr problem);
+
+      // Implement Modification::write().
+      void write(std::ostream &out) const;
+
+    private:
+      /// Lower or upper bound.
+      BoundType lu_;
+
+      /// The new value of the bound.
+      double newVal_;
+
+      /// The old value of the bound.
+      double oldVal_;
+
+      /// The constraint whose bounds are modified.
+      ConstraintPtr con_;
+  };
+
+  class ConBoundMod2;
+  typedef boost::shared_ptr<ConBoundMod2> ConBoundMod2Ptr;
+  typedef boost::shared_ptr<const ConBoundMod2> ConstConBoundMod2Ptr;  
+  typedef std::vector < ConBoundMod2Ptr > ConBoundMod2Vector;
+  typedef ConBoundMod2Vector::iterator ConBoundMod2Iter;
+  typedef ConBoundMod2Vector::const_iterator ConBoundMod2ConstIter;
+
+  /// Modification of a both bounds on a constraint.
+  class ConBoundMod2 : public Modification {
+    public:
+      /// Construct.
+      ConBoundMod2(ConstraintPtr con, double new_lb, double new_ub);
+
+      /// Destroy.
+      ~ConBoundMod2();
+
+      // Implement Modification::applyToProblem().
+      void applyToProblem(ProblemPtr problem);
+
+      // base class method.
+      ModificationPtr fromRel(RelaxationPtr, ProblemPtr ) const
+      {return ModificationPtr();};
+
+      /// Get the constraint whose bound is changed.
+      ConstraintPtr getCon() const;
+
+      /// Get new value of the bound.
+      double getNewLb() const;
+
+      /// Get new value of the bound.
+      double getNewUb() const;
+
+      // base class method.
+      ModificationPtr toRel(ProblemPtr, RelaxationPtr) const
+      {return ModificationPtr();};
+
+      // Implement Modification::undoToProblem().
+      void undoToProblem(ProblemPtr problem);
+
+      // Implement Modification::write().
+      void write(std::ostream &) const {};
+
+    private:
+      /// The new lower bound.
+      double newLb_;
+
+      /// The new upper bound.
+      double newUb_;
+
+      /// Old lower bound.
+      double oldLb_;
+
+      /// Old upper bound.
+      double oldUb_;
+
+      /// The constraint whose bounds are modified.
+      ConstraintPtr con_;
+  };
+
+
+  class ConMod;
+  typedef boost::shared_ptr<ConMod> ConModPtr;
+  typedef boost::shared_ptr<const ConMod> ConstConModPtr;  
+  typedef std::vector < ConModPtr > ConModVector;
+  typedef ConModVector::iterator ConModIter;
+  typedef ConModVector::const_iterator ConModConstIter;
+
+  /// Modification of a both bounds on a constraint.
+  class ConMod : public Modification {
+    public:
+      /// Construct.
+      ConMod(ConstraintPtr con, NonlinearFunctionPtr new_nlf);
+
+      /// Destroy.
+      ~ConMod();
+
+      // Implement Modification::applyToProblem().
+      void applyToProblem(ProblemPtr problem);
+
+      // base class method.
+      ModificationPtr fromRel(RelaxationPtr, ProblemPtr ) const
+      {return ModificationPtr();};
+
+      /// Get the constraint whose bound is changed.
+      ConstraintPtr getCon() const;
+
+      /// Get new nonlinearFunction 
+      NonlinearFunctionPtr getNewNlf() const;
+
+
+      // base class method.
+      ModificationPtr toRel(ProblemPtr, RelaxationPtr) const
+      {return ModificationPtr();};
+
+      // Implement Modification::undoToProblem().
+      void undoToProblem(ProblemPtr problem);
+
+      // Implement Modification::write().
+      void write(std::ostream &) const {};
+
+    private:
+      /// The new nonlinear function
+      NonlinearFunctionPtr newNlf_;
+
+
+      /// The old nonlinear function
+      NonlinearFunctionPtr oldNlf_;
+
+      /// The constraint whose bounds are modified.
+      ConstraintPtr con_;
+  };
+
+}
+
+#endif
+
+
+// Local Variables: 
+// mode: c++ 
+// eval: (c-set-style "k&r") 
+// eval: (c-set-offset 'innamespace 0) 
+// eval: (setq c-basic-offset 2) 
+// eval: (setq fill-column 78) 
+// eval: (auto-fill-mode 1) 
+// eval: (setq column-number-mode 1) 
+// eval: (setq indent-tabs-mode nil) 
+// End:
