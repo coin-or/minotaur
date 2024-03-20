7a1b374909d2f88aaf6547b795bff054b3a43a5c
Mustafa Vora <mustafa_vora712@yahoo.com>
2024-03-20 15:46:35 +0530

    Run only one heuristic at a time

--- src/base/PCBProcessor.cpp
+++ src/base/PCBProcessor.cpp
@@ -248,6 +248,9 @@ void PCBProcessor::process(NodePtr node, RelaxationPtr rel,
       // in root, in first iteration, run a heuristic. XXX: better management.
       for(HeurVector::iterator it = heurs_.begin(); it != heurs_.end(); ++it) {
         (*it)->solve(node, rel, s_pool);
+        if(s_pool->getBestSolutionValue() < INFINITY) {
+          break;
+        }
       }
       tightenBounds_(node, s_pool, sol, &sep_status);
     }
